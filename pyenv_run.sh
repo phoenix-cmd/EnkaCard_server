279287f5bdde9d8e419695670ff367f8660eb5a7
Iwak <157107911+KuntilBogel@users.noreply.github.com>
2024-06-02 05:32:50 +0000

    itu sih

--- pyenv_run.sh
+++ pyenv_run.sh
@@ -1,3 +1,8 @@
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+pyenv install 3.10.13
+pyenv global 3.10.13
+python -m pip install -r requirements.txt
+chmod +x run.sh
+./run.sh
